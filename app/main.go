cc40efce3dc5e6870fe69db673428525191e4cf7
Mark Milburn <mark.a.milburn@gmail.com>
2025-03-04 16:25:41 -0700

    Uncomment code for step one

--- app/main.go
+++ app/main.go
@@ -12,20 +12,15 @@ var _ = exec.Command
 
 // Usage: your_docker.sh run <image> <command> <arg1> <arg2> ...
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
+	command := os.Args[3]
+	args := os.Args[4:len(os.Args)]
 
-	// Uncomment this block to pass the first stage!
-	//
-	// command := os.Args[3]
-	// args := os.Args[4:len(os.Args)]
-	//
-	// cmd := exec.Command(command, args...)
-	// output, err := cmd.Output()
-	// if err != nil {
-	// 	fmt.Printf("Err: %v", err)
-	// 	os.Exit(1)
-	// }
-	//
-	// fmt.Println(string(output))
+	cmd := exec.Command(command, args...)
+	output, err := cmd.Output()
+	if err != nil {
+		fmt.Printf("Err: %v", err)
+		os.Exit(1)
+	}
+
+	fmt.Println(string(output))
 }
